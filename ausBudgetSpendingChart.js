a842d318242cfde0123b529518a0cb9857718894
dred0006 <dred0006@student.monash.edu>
2024-10-14 11:09:18 +1100

    Adding new charts to visualisation

---
+++ ausBudgetSpendingChart.js
@@ -0,0 +1,2 @@
+var vg_3 = "ausBudgetSpendingChart.vg.json";
+vegaEmbed("#ausBudgetSpendingChart", vg_3).then(function(result) {}).catch(console.error);
---
+++ fundingByGenreChart.js
@@ -0,0 +1,2 @@
+var vg_4 = "fundingByGenreChart.vg.json";
+vegaEmbed("#fundingByGenreChart", vg_4).then(function(result) {}).catch(console.error);
